b49cee493bf9d06eaa30f557c5a71cc016c87c2c
Lina <117147821+LinA636@users.noreply.github.com>
2023-12-03 10:19:33 +1300

    match up files to successfully create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(username: params[:user_name], email: params[:user_email], password: params[:password])
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   
     if @user.save
       redirect_to new_user_path
