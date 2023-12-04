cbc480695942992dc3785f0d62440c440b7230f8
Lina <117147821+LinA636@users.noreply.github.com>
2023-12-04 15:18:40 +1300

    create show page for created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
+  # GET /users/new
   def new
     @user = User.new
   end
   
+  # POST /users
   def create
     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
   
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
   end
 
   private
